2a69f904ef946ccae5df8ad101476dc5cb40b52c
Luis Felipe Diaz Valbuena <luis.felipe.diaz.v@gmail.com>
2020-07-16 15:02:26 -0500

    ajuste en pruebas de gestion de estudiantes

--- src/app/gestion-estudiantes/gestion-estudiantes.service.spec.ts
+++ src/app/gestion-estudiantes/gestion-estudiantes.service.spec.ts
@@ -1,12 +1,33 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { GestionEstudiantesService } from './gestion-estudiantes.service';
 
 describe('GestionEstudiantesService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: GestionEstudiantesService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [GestionEstudiantesService]
+    }));
+
+  beforeEach(() => {
+    service = TestBed.inject(GestionEstudiantesService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
 
   it('should be created', () => {
-    const service: GestionEstudiantesService = TestBed.get(GestionEstudiantesService);
     expect(service).toBeTruthy();
+  });
+
+  it('debe "obtenerEstudiantes" llamar al servicio de "inserte url" y ser una peticion "get"', () => {
+    service.obtenerEstudiantes().subscribe();
+
+    const peticion = httpMock.expectOne('inserte url equisde');
+
+    expect(peticion).toBeTruthy();
+    expect(peticion.request.method).toEqual('GET');
   });
 });
